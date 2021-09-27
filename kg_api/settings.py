21455ff5aab6d1d883992266ba144043d8d4d3ca
AbrorjonDev <axmadov.git@gmail.com>
2021-09-27 17:24:45 +0500

    API Docs Added

--- kg_api/settings.py
+++ kg_api/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 REST_FRAMEWORK = {
-
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',

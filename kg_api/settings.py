c807044ba4dcc9b7c064f6b8dc6080b98e3d5b03
abrorjon <axmadov.git@gmail.com>
2021-10-17 21:25:51 +0500

    post objects are added to kg model objects

--- kg_api/settings.py
+++ kg_api/settings.py
@@ -102,7 +102,7 @@ CORS_ALLOW_HEADERS = [
     "x-requested-with",
 ]
 
-CSRF_TRUSTED_ORIGINS = 'bogcha.herokuapp.com'
+CSRF_TRUSTED_ORIGINS = 'https://bogcha.herokuapp.com/'
 
 CORS_REPLACE_HTTPS_REFERER = True
 
